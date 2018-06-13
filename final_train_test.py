24334627be19755e2c7d6e9ce580724a52fc8683
= <=>
2018-06-13 22:07:18 +0900

    Modified final_trina_test.py now save model files with input argument --name

--- final_train_test.py
+++ final_train_test.py
@@ -3,12 +3,14 @@ import sys
 import os
 import numpy as np
 import librosa
+from sklearn.preprocessing import StandardScaler
 from final_feature_summary import *
 import librosa.display
 import numpy, scipy, matplotlib.pyplot as plt, IPython.display as ipd
 import pickle
 import random
 from sklearn.externals import joblib
+import argparse
 
 FEATURE_PATH = './dataset/features/full'
 SHUFFLE = True
@@ -64,6 +66,7 @@ def make_dataset(data_unit=DATA_UNIT, train_ratio=0.6, valid_ratio=0.2):
         valid_Y = [el[2] for el in feature_list[int(n_total*train_ratio): int(n_total*(train_ratio+valid_ratio))]]
         test_X = [el[0] for el in feature_list[int(n_total*(train_ratio+valid_ratio)):]]
         test_Y = [el[2] for el in feature_list[int(n_total*(train_ratio+valid_ratio)):]]
+
     elif data_unit == 'Pattern':
         random.shuffle(feature_list)
         random.shuffle(pattern_list)
@@ -103,13 +106,14 @@ def make_dataset(data_unit=DATA_UNIT, train_ratio=0.6, valid_ratio=0.2):
                                                id_list[int(n_total * (train_ratio + valid_ratio)):, 0])
     return train_X, train_Y, valid_X, valid_Y, test_X, test_Y
 
+
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--name', default='test')
+    parser.add_argument('--unit', default='id')
+    args = parser.parse_args()
 
     train_X, train_Y, valid_X, valid_Y, test_X, test_Y = make_dataset()
-
-    #####################
-    from sklearn.preprocessing import StandardScaler
-
 
     print ("Shuffle: "+str(SHUFFLE)+" Data Unit: "+DATA_UNIT)
     print (train_X.shape)
@@ -158,71 +162,9 @@ if __name__ == '__main__':
         print (MLPT)
         pp=classification_report(test_Y, predictions)
 
-    filename = 'finalized_model.sav'
+    filename = '{}_model.sav'.format(args.name)
     pickle.dump(mlp, open(filename, 'wb'))
 
     # loaded_model = pickle.load(open('./finalized_model.sav', 'rb'))
     # result = loaded_model.score(test_X, test_Y)
     # print(result)
-
-
-
-
-#############################
-
-
-
-
-
-
-
-
-    ##########SGD
-
-    ###############feature normalizaiton original
-
-
-
-
-    # train_X = train_X.T
-    # train_X_mean = np.mean(train_X, axis=0)
-    # train_X = train_X - train_X_mean
-    # train_X_std = np.std(train_X, axis=0)
-    # train_X = train_X / (train_X_std + 1e-5)
-    #
-    # valid_X = valid_X.T
-    # valid_X = valid_X - train_X_mean
-    # valid_X = valid_X/(train_X_std + 1e-5)
-    #
-    # # training model
-    # alphas = [0.0001, 0.001, 0.01, 0.1, 1, 10]
-    #
-    # model = []
-    # valid_acc = []
-    # for a in alphas:
-    #     clf, acc = train_model(train_X, train_Y, valid_X, valid_Y, a)
-    #     model.append(clf)
-    #     valid_acc.append(acc)
-    #
-    # # choose the model that achieve the best validation accuracy
-    # final_model = model[np.argmax(valid_acc)]
-    #
-    # # now, evaluate the model with the test set
-    # test_X = test_X.T
-    # test_X = test_X - train_X_mean
-    # test_X = test_X/(train_X_std + 1e-5)
-    # test_Y_hat = final_model.predict(test_X)
-    #
-    # accuracy = np.sum((test_Y_hat == test_Y))/test_num_audio*100.0
-    # print ('test accuracy = ' + str(accuracy) + ' %')
-    #
-    # filename = 'finalized_model.sav'
-    # pickle.dump(final_model, open(filename, 'wb'))
-    #
-    #
-
-
-
-##################
-
-
